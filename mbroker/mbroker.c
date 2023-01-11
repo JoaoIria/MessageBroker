d913d30078a85f0d66d6cc78afbc8d95055ce038
joaoafs <joaoamadeusantos@gmail.com>
2023-01-11 19:46:17 +0000

    starting

--- mbroker/mbroker.c
+++ mbroker/mbroker.c
@@ -1,37 +1,79 @@
-#include "logging.h"
+#include <stdio.h>
+#include <pthread.h>
 #include <string.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-char msg[] = "mensagem de teste";
-char buffer[1024];
+#define MAX_THREADS 100 /* 100 for example, but can change */
+
+const char* register_pipe_name;
+int max_sessions;
+pthread_t session_threads[MAX_THREADS];
+int num_threads = 0;
+pthread_mutex_t mb_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void* session_handler(void* session_pipe_name) {
+    const char* pipe_name = (const char*) session_pipe_name;
+    int fd = open(pipe_name, O_RDONLY);
+
+    /* Initializes a user session */
+
+    close(fd);
+    free((void*) pipe_name);
+    pthread_mutex_lock(&mb_mutex);
+    num_threads--;
+    pthread_mutex_unlock(&mb_mutex);
+    return NULL;
+}
 
 int main(int argc, char **argv) {
+    if (argc < 3) {
+        printf("Usage: %s <register_pipe_name> <max_sessions>\n", argv[0]); /*tirar*/
+        return -1;
+    }
 
-    (void)argc;
-    (void)argv;
-
-    int max_sessions;
-    char register_pipe_name[1024];
-    int i;
-
-    strcpy(register_pipe_name, argv[1]);
+    strcpy(register_pipe_name,argv[1]);
     max_sessions = atoi(argv[2]);
 
-    printf("%s %d",register_pipe_name, max_sessions);
-
-    mkfifo(register_pipe_name, max_sessions);
-    int fd = open(register_pipe_name, O_RDONLY);
-     
-    if (fd == -1){
+    if (mkfifo(register_pipe_name, 0666) < 0) {
+        printf("Error creating register pipe\n"); /*tirar*/
         return -1;
     }
-    
-    if (read(fd, &buffer[i], sizeof(int))== -1){
-        return -1;
 
+    int fd = open(register_pipe_name, O_RDONLY | O_NONBLOCK);
+    if (fd < 0) {
+        printf("Error opening register pipe\n"); /*tirar*/
+        return -1;
+    }
+
+    while (1) {
+        char session_pipe_name[256];
+        int n = read(fd, session_pipe_name, sizeof(session_pipe_name));
+        if (n < 0) {
+            continue;
+        }
+        session_pipe_name[n] = '\0';
+        /* allows for functions like printf and strlen 
+        to know where the string ends and stops reading from it */ 
+
+        /* Check if there is space for a new thread */
+        pthread_mutex_lock(&mb_mutex);
+        if (num_threads >= max_sessions) {
+            pthread_mutex_unlock(&mb_mutex);
+            /* wait for a thread to finish */
+            continue;
+        }
+        num_threads++;
+        pthread_mutex_unlock(&mb_mutex);
+
+        char* session_pipe_name_copy = strdup(session_pipe_name);
+        pthread_create(&session_threads[num_threads-1], NULL, session_handler, session_pipe_name_copy);
     }
     close(fd);
-
+    for (int i = 0; i < num_threads; i++)
+        pthread_join(session_threads[i], NULL);
+    unlink(register_pipe_name);
     return 0;
 }
--- publisher/pub.c
+++ publisher/pub.c
@@ -1,9 +1,50 @@
-#include "logging.h"
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
-    fprintf(stderr, "usage: pub <register_pipe_name> <box_name>\n");
-    WARN("unimplemented"); // TODO: implement
-    return -1;
+    /* check for correct number of command line arguments */
+    if (argc < 4) {
+        printf("Usage: %s <register_pipe_name> <session_pipe_name> <box_name>\n", argv[0]);
+        return -1;
+    }
+
+    const char* register_pipe_name = argv[1];
+    const char* session_pipe_name = argv[2];
+    const char* box_name = argv[3];
+
+    /* open the register pipe for writing */
+    int register_pipe_fd = open(register_pipe_name, O_WRONLY);
+    /* check for error opening the register pipe */
+    if (register_pipe_fd < 0) {
+        printf("Error opening register pipe\n"); /* Tirar */
+        return -1;
+    }
+
+    /* format message to send to mbroker */
+    char message = "[ code = 9 (uint8_t) ] | [ message (char[1024]) ]";
+
+    /* send the message to the mbroker via the register pipe */
+    write(register_pipe_fd, message, strlen(message));
+    close(register_pipe_fd);
+
+
+    /* open the session pipe for writing 
+    int session_pipe_fd = open(session_pipe_name, O_WRONLY);
+    /* check for error opening the session pipe 
+    if (session_pipe_fd < 0) {
+        printf("Error opening session pipe\n");
+        return -1;
+    }
+
+    /* publisher can now write messages to the session pipe 
+    while(1){
+        char msg[512];
+        printf("Enter the message to publish:");
+        scanf("%s", msg);
+        write(session_pipe_fd, msg, strlen(msg));
+    }
+    close(session_pipe_fd);*/
+    return 0;
 }
